2feb6d7899d114554b33194a7a7c6fc390d9ff00
superducktoes <nroy@TNS3086L.home>
2016-12-29 10:59:12 -0500

    initial check
    added initial check to see if the script was configured already

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#checks to see if the files have been modified already.
+#if not then we'll replace the variables.
+if grep -r -q "accessKeyReplace" ./nessusScan.py
+then
+    echo "Let's configured a few variables for the scripts"
+else
+    echo "Looks like the files have already been modified"
+    exit 1
+fi
+
 echo "Enter the access key: "
 read accessKey
 echo "Enter the secret key: "
